5ff61b1993e30f27e82622439c135b8496e35207
Tamil-durga-Pari <94234015+Tamil-durga-Pari@users.noreply.github.com>
2021-12-02 20:31:25 +0530

    Update distance-measurement.h

--- 3_Implementation/inc/distance-measurement.h
+++ 3_Implementation/inc/distance-measurement.h
@@ -1,1 +1,20 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#define F_CPU 1000000   /* clock frequency of MCU id 1MHz */
+/**
+ * @file distance-measurement.h
+ * @author tamildurga ()
+ * @brief Configuration file to define pins and ports for the interfaced peripherals
+ * @version 0.1
+ * @date 2021-012-1
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+  
+#include <util/delay.h>
+#include <stdlib.h>
+
+#define enable            
+#define registerselection 
 
